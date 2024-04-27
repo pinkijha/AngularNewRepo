a1dbe12e8e9bac53e4773e76a157edffaecc862b
“pinki <pinkijha1310@email.com>
2024-04-28 00:03:55 +0530

    edit login page

--- angularProjects/ecommerce/bigbasket/src/app/pages/admin/login/login.component.ts
+++ angularProjects/ecommerce/bigbasket/src/app/pages/admin/login/login.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
-
+  constructor(private titleService: Title){
+    this.titleService.setTitle('bigBasket - login')
+  }
 }
