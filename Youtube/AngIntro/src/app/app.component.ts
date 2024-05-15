21c263fff2f3a722eb74efbd946285eb6002793f
“pinki <pinkijha1310@email.com>
2024-05-16 01:08:12 +0530

    angular change detection

--- Youtube/AngIntro/src/app/app.component.ts
+++ Youtube/AngIntro/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  postarr : Array<string> = ['post1','post2','post3','post4','post5','post6',];
-objArray : any = [
+objArray : Array<any> = [
   {id: 1, postTitle: 'post1'},
   {id: 2, postTitle: 'post2'},
   {id: 3, postTitle: 'post3'},
@@ -15,5 +15,13 @@ objArray : any = [
   {id: 5, postTitle: 'post5'},
   {id: 6, postTitle: 'post6'},
 ]
+
+addNew(){
+  this.objArray.push({id:7,postTitle: 'post7'});
+}
+
+remove(){
+  this.objArray.pop();
+}
 
 }
