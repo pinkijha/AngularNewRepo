e817e4aea9e9d93e962d6d85c6e0bdb8af338f60
“pinki <pinkijha1310@email.com>
2024-05-16 00:37:40 +0530

    fetch object aaray using ngFor directive

--- Youtube/AngIntro/src/app/app.component.ts
+++ Youtube/AngIntro/src/app/app.component.ts
@@ -7,8 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  postarr : Array<string> = ['post1','post2','post3','post4','post5','post6',];
+objarr : Array<object> = [
+  {id: 1, postTitle: 'post1'},
+  {id: 2, postTitle: 'post2'},
+  {id: 3, postTitle: 'post3'},
+  {id: 4, postTitle: 'post4'},
+  {id: 5, postTitle: 'post5'},
+  {id: 6, postTitle: 'post6'},
+]
 
- constructor(){
-
- }
 }
