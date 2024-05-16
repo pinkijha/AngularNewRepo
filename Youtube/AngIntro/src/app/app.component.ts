93a4d208c4d1afac7ca84c8cb974d6c531d9302e
“pinki <pinkijha1310@email.com>
2024-05-16 15:24:27 +0530

    ngif directive

--- Youtube/AngIntro/src/app/app.component.ts
+++ Youtube/AngIntro/src/app/app.component.ts
@@ -7,22 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
  postarr : Array<string> = ['post1','post2','post3','post4','post5','post6',];
-objArray : Array<any> = [
-  {id: 1, postTitle: 'post1'},
-  {id: 2, postTitle: 'post2'},
-  {id: 3, postTitle: 'post3'},
-  {id: 4, postTitle: 'post4'},
-  {id: 5, postTitle: 'post5'},
-  {id: 6, postTitle: 'post6'},
-]
-
-
+objArray : Array<any> = []
 addNew(){
   this.objArray.push({id:7,postTitle: 'post7'});
 }
-
 remove(index: any){
 this.objArray.splice(index, 1)
 }
-
 }
